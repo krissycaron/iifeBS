01797d515bc85561ef49b9386cc5d82c23cf6072
Kristin Caron <krissycaron@gmail.com>
2017-03-24 14:25:20 -0500

    working on adding data to table

--- walk.js
+++ walk.js
@@ -8,8 +8,8 @@ var table = document.getElementById("tableList");
 var clearBtn = document.getElementById("clearBtn");
 clearBtn.addEventListener("click", cleaForm);
 
-var donateBtn = document.getElementById("donateBtn");
-donateBtn.addEventListener("click", submitForm);
+// var donateBtn = document.getElementById("donateBtn");
+// donateBtn.addEventListener("click", submitForm);
 
 //Button functions (clear button)
 function cleaForm (){
@@ -18,25 +18,36 @@ function cleaForm (){
 }
 
 // get data from form and add donor object to donor list array
-function submitForm () {
-	var teamName = teamNameInput.value;
-	var donorAmount = donationAmountInput.value;
-	cleaForm();
+// function submitForm () {
+// 	var teamName = teamNameInput.value;
+// 	var donorAmount = donationAmountInput.value;
+// 	cleaForm();
 
-	var newDonor = {"name": teamName, 
-					"donation" : donorAmount,
-					}
+// 	var newDonor = {"name": teamName, 
+// 					"donation" : donorAmount,
+// 					}
 
-	var tableInput = WalkAthon.getdonorArray();
-	console.log("1");
-}
+// 	var tableInput = WalkAthon.getdonorArray();
+// 	displayTable();
+// }
 
 function displayTable (){
-	console.log("2");
+	table.innerHTML = "";
+
+	var tableColumnNames = document.createElement("td");
+	tableColumnNames.innerHTML = 
+		`<tr>
+	        <th scope="col">Team Name</th>
+	        <th scope="col">Pleadge</th>
+    	</tr>`;
+	table.insertBefore(displayTable);
+
+
+
 }
 
 
-submitForm();
+// submitForm();
 displayTable();
 
 
