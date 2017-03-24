d56df9aba6750321dfa053eca1e16bbf9b8dde93
Kristin Caron <krissycaron@gmail.com>
2017-03-23 21:06:29 -0500

    started variable

---
+++ donor.js
@@ -0,0 +1,9 @@
+// The donor information should be stored as JS objects that contain their content type as keys and the submitted information as the value. {name: "John Donut", pledge: 70}
+var donors = (function(donor)) {
+	//array to add all donors to 
+	var donorArray = [];
+
+});
+
+
+
---
+++ walk.js
@@ -0,0 +1,15 @@
+//variables to hold user input.
+var teamName = document.getElementById("teamInput");
+var donationAmount = document.getElementById("donateInput");
+
+
+
+
+
+
+
+
+// var WalkAThon = (function () {
+
+
+// });
