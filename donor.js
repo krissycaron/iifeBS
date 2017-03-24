c1df1dd93d3bf109f20f8230caefdafb96040512
Kristin Caron <krissycaron@gmail.com>
2017-03-23 22:29:51 -0500

    started to create submit for function

--- donor.js
+++ donor.js
@@ -1,5 +1,5 @@
 // The donor information should be stored as JS objects that contain their content type as keys and the submitted information as the value. {name: "John Donut", pledge: 70}
-var donors = (function(donor)) {
+var WalkAThon = (function()) {
 	//array to add all donors to 
 	var donorArray = [];
 
--- walk.js
+++ walk.js
@@ -1,13 +1,29 @@
 //variables to hold user input.
-var teamName = document.getElementById("teamInput");
-var donationAmount = document.getElementById("donateInput");
+var teamNameInput = document.getElementById("teamInput");
+var donationAmountInput = document.getElementById("donateInput");
 
+//Button Variables and event  Listeners
+var clearBtn = document.getElementById("clearBtn");
+clearBtn.addEventListener("click", cleaForm);
+// console.log(clearBtn);
 
+var donateBtn = document.getElementById("donateBtn");
+donateBtn.addEventListener("click", submitForm);
 
+function cleaForm (){
+	teamNameInput.value = "";
+	donationAmountInput.value = ""; 
+}
 
+// get data from form and add donor object to donor list array
+function submitForm () {
+	var teamName = teamNameInput.value;
+	var donorAmount = donationAmountInput.value;
 
-
-
+	var newDonor = {"name": teamName, 
+					"donation" : donorAmount,
+					}
+}
 
 // var WalkAThon = (function () {
 
