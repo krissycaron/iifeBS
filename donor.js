f02f43c13fae046156ed87394f6a7defed38337e
Kristin Caron <krissycaron@gmail.com>
2017-03-24 13:32:00 -0500

    started on DOM functions and inuts

--- donor.js
+++ donor.js
@@ -17,4 +17,3 @@ var WalkAThon = (function(donor)) {
 })(donor || {});
 
 
-
--- walk.js
+++ walk.js
@@ -2,10 +2,11 @@
 var teamNameInput = document.getElementById("teamInput");
 var donationAmountInput = document.getElementById("donateInput");
 
+var table = document.getElementById("tableList");
+
 //Button Variables and event Listeners
 var clearBtn = document.getElementById("clearBtn");
 clearBtn.addEventListener("click", cleaForm);
-// console.log(clearBtn);
 
 var donateBtn = document.getElementById("donateBtn");
 donateBtn.addEventListener("click", submitForm);
@@ -20,8 +21,23 @@ function cleaForm (){
 function submitForm () {
 	var teamName = teamNameInput.value;
 	var donorAmount = donationAmountInput.value;
+	cleaForm();
 
 	var newDonor = {"name": teamName, 
 					"donation" : donorAmount,
 					}
+
+	var tableInput = WalkAthon.getdonorArray();
+	console.log("1");
 }
+
+function displayTable (){
+	console.log("2");
+}
+
+
+submitForm();
+displayTable();
+
+
+
